c0a60a9058554b5260567c63f1d2474f7cf82003
Eduard Hirsch <eduard.hirsch@fh-salzburg.ac.at>
2018-08-30 21:57:46 +0200

    first network initalized with tutorial setup

--- chain/initNetwork.sh
+++ chain/initNetwork.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+# network is using the tutorial certs and pathes
 
-echo TODO
+composer archive create -t dir -n .
+
+pem_admin_file=/home/vagrant/fabric-dev-servers/fabric-scripts/hlfv11/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem
+
+cert_admin_file=/home/vagrant/fabric-dev-servers/fabric-scripts/hlfv11/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/114aab0e76bf0c78308f89efc4b8c9423e31568da0c340ca187a9b17aa9a4457_sk
+
+composer card create -p connection.json -u PeerAdmin -c $pem_admin_file -k $cert_admin_file -r PeerAdmin -r ChannelAdmin
+
+composer card import -f PeerAdmin@fabric-network.card
+
+composer network install -c PeerAdmin@fabric-network -a sardex-open-network\@0.0.1.bna
+
+composer network start --networkName sardex-open-network --networkVersion 0.0.1 -A admin -S adminpw -c PeerAdmin@fabric-network
+
+composer card import -f admin@sardex-open-network.card
--- chain/lib/transactions.js
+++ chain/lib/transactions.js
@@ -1,9 +1,36 @@
+const TransferLabel_CreditTransfer = "CreditTransfer";
+const TransferLabel_DebitTransfer = "DebitTransfer";
+ 
  /**
  * Sample transaction processor function.
  * @param {net.sardex.interlace.CreditTransfer} transfer The sample transaction instance.
  * @transaction
  */
-function CreditTransfer(transfer) {
+async function CreditTransfer(transfer) {
+	//some error checking
+	if (transfer.transferLabel != TransferLabel_CreditTransfer) {
+		console.log("Wrong transfer label");
+		return;
+	}
+	if (transfer.amount <= 0) {
+		console.log("Transfer amount must be a positive value.");
+		return;
+	}
+	if (transfer.senderAccount.balance < transfer.amount) {
+		console.log("Transfer amount " + transfer.amount + 
+					" is bigger than the available balance of " + transfer.senderAccount.balance);
+		return;
+	}
+	
+	//move money
+	transfer.senderAccount.balance -= transfer.amount;
+	transfer.recipientAccount.balance += transfer.amount;
+	
+    let assetRegistry = await getAssetRegistry('net.sardex.interlace.CreditTransfer');
+
+    // persist the state of the account as well as accountReceive
+    await assetRegistry.update(transfer.senderAccount);
+    await assetRegistry.update(transfer.recipientAccount);
 }
 
 /**
@@ -11,5 +38,11 @@ function CreditTransfer(transfer) {
  * @param {net.sardex.interlace.DebitTransfer} transfer The sample transaction instance.
  * @transaction
  */
-function DebitTransfer(transfer) {
+async function DebitTransfer(transfer) {
+	//some error checking
+	if (transfer.transferLabel != TransferLabel_DebitTransfer) {
+		console.log("Wrong transfer label");
+		return;
+	}
+	console.log("not implemented");
 }
