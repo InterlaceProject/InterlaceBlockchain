a16c4b05313044575d03e6623b8659f4b39d05ee
Eduard Hirsch <eduard.hirsch@fh-salzburg.ac.at>
2018-09-18 22:07:01 +0200

    refactored files

--- chain/lib/transactions.js
+++ chain/lib/transactions.js
@@ -1,37 +1,5 @@
 'use strict';
 
-var Unit = Object.freeze({
-  'Euro': 'Euro',
-  'SRD': 'SRD'
-});
-
-var GroupType = Object.freeze ({
-  'welcome': 'welcome',
-  'retail': 'retail',
-  'company': 'company',
-  'full': 'full',
-  'employee': 'employee',
-  'on_hold': 'on_hold',
-  'MNGR': 'MNGR',
-  'consumer': 'consumer',
-  'consumer_verified': 'consumer_verified'
-});
-
-var Operation = Object.freeze ({
-  'Credit': 'Credit',
-  'Debit': 'Debit'
-});
-
-var AccountType = Object.freeze({
-  'CC':'CC',
-  'DOMU': 'DOMU',
-  'MIRROR': 'MIRROR',
-  'Income': 'Income',
-  'Prepaid': 'Prepaid',
-  'Bisoo': 'Bisoo',
-  'Topup': 'Topup'
-});
-
 var accTTree = {
   credit: {
     SRD: {
