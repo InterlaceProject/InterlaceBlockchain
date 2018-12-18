d5f540dcafdce121415933a620b3eb624f9810e1
Eduard Hirsch <eduard.hirsch@fh-salzburg.ac.at>
2018-12-18 10:00:40 +0100

    small fixes

--- quickSetup.sh
+++ quickSetup.sh
@@ -3,5 +3,13 @@ fabric/deletePlaygroundCards.sh &&
 	fabric/teardownFabric.sh &&
 	fabric/startFabric.sh &&
 	cd chain/ &&
-	./initNetwork.sh &&
-	composer transaction submit -c admin@sardex-open-network -d  '{ "$class": "net.sardex.interlace.InitBlockchain" }'
+	./initNetwork.sh
+
+if ! [ $? -eq 0 ]; then
+    >&2 echo "Error during setting up network."
+    exit 1
+fi
+
+echo "Filling Blockchain with demo data ..."
+composer transaction submit -c admin@sardex-open-network -d  '{ "$class": "net.sardex.interlace.InitBlockchain" }'
+echo "Done."
